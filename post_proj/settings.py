a01fd242b21caddb170cfd94b8937e2dae90bda4
yuenfu001 <liuyuenfu@gmail.com>
2025-04-26 17:59:52 +0100

    postgres

--- post_proj/settings.py
+++ post_proj/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
-
+from post_proj.postgresql import post
+poster = post()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,10 +77,21 @@ WSGI_APPLICATION = "post_proj.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': poster.db_name,
+        'USER': poster.user,
+        'PASSWORD': poster.host,
+        'HOST': 'd03v9ridbo4c73c8cge0-a.oregon-postgres.render.com',
+        'PORT':poster.port
     }
 }
 
